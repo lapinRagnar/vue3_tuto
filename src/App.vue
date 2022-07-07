4f6f7299a120f4995043d1e175956b83768c9455
lapinRagnar <lapinragnar@gmail.com>
2022-07-07 10:40:29 +0200

    6 -formulaire  - v-model - 2 way binding

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,15 @@
 <template >
-
-  <h1>nom: {{nom}}</h1>
-  <h1>compteur # {{count}}</h1>
-
   <div>
-    <button @click="modifierNom(), ajouter(4, $event)">ajouter</button>
+    <pre>
+      {{ JSON.stringify(formValues, null, 2)}}
+    </pre>
   </div>
-
+  <form action="">
+    <div>
+      <label for="name">name</label>
+      <input type="text" id="name" v-model="formValues.name">
+    </div>
+  </form>
 </template>
 
 <script>
@@ -14,28 +17,50 @@ export default {
   name: 'App',
   data(){
     return {
-      count: 0,
-      nom: 'rabitro'
+      formValues: {
+        name:''
+      }
     }    
   },
   methods: {
-    ajouter(num, event){
-      this.count += num
-      console.log('evenement object', event)
-    },
-    modifierNom(){
-      this.nom = 'ra la la la la!'
-    }
+
   }
 }
 </script>
 
 <style>
+
+label{
+  font-weight: bold;
+  display: flex;
+  margin-bottom: 5px;
+}
+
+input + label {
+  font-weight: bold;
+  display: inline-flex;
+  margin-right: 20px;
+}
+
+input[type='text'], 
+textarea,
+select{
+  display: block;
+  width: 400px;
+  padding: 6px 12px;
+  font-size: 14px;
+  line-height: 1.42;
+  color: #555;
+  background-color: #fff;
+  background-image: none;
+}
+
 #app {
+
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  /* text-align: center; */
   color: #2c3e50;
   margin-top: 60px;
 }
