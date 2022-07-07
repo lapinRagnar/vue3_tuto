355fe1a6588a984f6288736bdd9e79d3fa67c419
lapinRagnar <lapinragnar@gmail.com>
2022-07-07 07:05:43 +0200

    2 - directive v-if, v-else, v-else-if, v-show

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <h2 v-bind:style="monStyle">ajout inline style - avec un objet</h2>
+  <div v-if="num === 0">number is 0</div>
 </template>
 
 <script>
@@ -7,13 +7,7 @@ export default {
   name: 'App',
   data(){
     return {
-      monStyle: {
-        color: 'red',
-        height: '100px',
-        width: '300px',
-        borderRadius: '10px',
-        'background-color': 'orange'
-      }
+      num: 0
     }
   }
 }
@@ -28,15 +22,6 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
-.sold_out{
-  color: #95bc4c;
-  font-size: 40px;
-  height: 100px;
-}
 
-.new{
-  color: rebeccapurple;
-  background-color: #95bc4c;
-}
 
 </style>
