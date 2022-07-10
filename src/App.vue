f05753f9f47acafb9810d29654ee0b457ccd6b21
lapinRagnar <lapinragnar@gmail.com>
2022-07-10 10:30:14 +0200

    8 - watchers permet de voir n'inporte quelle donnée ou computed property et execute des codes pour modifier les données

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,11 @@
 <template >
-  <h2>computed total - {{ total }} </h2>
-  <h2>Method total - {{ getTotal() }} </h2>
-  <button @click="items.push({id: 4, title: 'essai', price: 1500})" >ajouter</button>
-
-  <!--Afficher les produits qui a un prix superieur à 200 euro-->
-  <h2 style="border-bottom: 1px solid blue;">prix supérieur à 200 euro avec v-for</h2>
-  <template v-for="item in items" :key="item.id">
-    <h4 v-if="item.price > 200">{{ item.title}} - {{ item.price}}</h4>
-  </template>
-
-  <!--Afficher les produits qui a un prix superieur à 200 euro-->
-  <h2 style="border-bottom: 1px solid blue;">prix supérieur à 200 euro avec v-for et computed property</h2>
-  <h4 v-for="item in expensiveItems" :key="item.id">{{ item.title}} ## {{ item.price}}</h4>
-
-  <!--getter et setter avec computed property-->
-  <h2 style="border-bottom: 1px solid blue;">getter et setter avec computed property</h2>
-  <h4>computed fullName - {{ fullName }}</h4>
-  <button @click="changeFullName">Changer le nom</button>
+  <h4>volume tracker - (0-20)</h4>
+  <p>current volume - {{ volume }}</p>
+  <p style="background: red; color white;" v-if="isAlarme">le volume est trop haut, baisse le stp...</p>
+  <div>
+    <button @click="volume += 2">increase</button>
+    <button @click="volume -= 2">decrease</button>
+  </div>
 
 </template>
 
@@ -25,50 +14,18 @@ export default {
   name: 'App',
   data(){
     return {
-      firstName: 'bruce',
-      lastName: 'wayne',
-      items: [
-        {
-          id: 1,
-          title: 'tv',
-          price: 200
-        },
-        {
-          id: 2,
-          title: 'phone',
-          price: 3500
-        },
-        {
-          id: 3,
-          title: 'laptop',
-          price: 400
-        },
-    ]
+      volume: 0,
+      isAlarme: false
     }    
   },
-  methods: {
-    getTotal(){
-      return this.items.reduce((total, curr) => (total = total + curr.price), 0) 
-    },
-    changeFullName(){
-      this.fullName = 'clark kent'
-    }
-  },
-  computed: {
-    total(){
-      return this.items.reduce((total, curr) => (total = total + curr.price), 0) 
-    },
-    expensiveItems(){
-      return this.items.filter(item => item.price > 200)
-    },
-    fullName: {
-      get(){
-        return `${this.firstName} ** ${this.lastName}`
-      },
-      set(value){
-        const names = value.spit(' ')
-        this.firstName = names[0]
-        this.lastName = names[1]
+  methods: {},
+  computed: {},
+  watch: {
+    volume(newValue, oldValue){
+      if (newValue> oldValue && newValue === 16) {
+        this.isAlarme = true
+      }else {
+        this.isAlarme = false
       }
     }
   }
