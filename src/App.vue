d0c939b792891e83f33ef21e14fdd03e743d1cfe
lapinRagnar <lapinragnar@gmail.com>
2022-07-13 11:34:53 +0200

    16 - http get request - api call - axio
    site pour api :
    https://jsonplaceholder.typicode.com/

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,19 @@
 <template >
-
-  <h4>bonjour parent</h4>
-  <button @click="activeTab = 'TabA'">Tab A</button>
-  <button @click="activeTab = 'TabB'">Tab B</button>
-  <button @click="activeTab = 'TabC'">Tab C</button>
-  <keep-alive>
-    <component :is="activeTab" />
-  </keep-alive>
-
-  <!-- <TabA v-if="activeTab === 'TabA'"></TabA>
-  <TabB v-if="activeTab === 'TabB'"></TabB>
-  <TabC v-if="activeTab === 'TabC'"></TabC> -->
+  <div>app compo</div>
+  <PostList/>
 
 </template>
 
 <script>
 
-import TabA from './components/TabA.vue'
-import TabB from './components/TabB.vue'
-import TabC from './components/TabC.vue'
+
+import PostList from './components/PostList.vue'
 
 export default {
   name: 'App',
-  components: { TabA, TabB, TabC },
+  components: { PostList },
   data(){
-    return {
-      activeTab: 'TabA'
-    }    
+    return {}    
   },
   methods: {},
   computed: {},
@@ -39,9 +26,5 @@ export default {
   #app {
     font-family: Avenir, Helvetica, Arial, sans-serif;
   }
-  h4{
-    color: orange;
-  }
-
 
 </style>
---
+++ src/components/PostList.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>post list compo</div>
+  <button @click="getPosts">Load Post</button>
+  <h2 v-if="errorMsg">{{errorMsg}}</h2>
+  <div v-for="post in posts" :key="post.id">
+    <h3>{{post.id}} ## {{post.title}}</h3>
+    <p style="boder-bottom: 1px style orange">{{post.body}}</p>
+  </div>
+</template>
+
+<script>
+
+import axios from 'axios'
+
+export default {
+  name: 'PostList',
+  data(){
+    return {
+      posts: [],
+      errorMsg: ''
+    }
+  },
+  methods: {
+    getPosts(){
+      axios.get('https://jsonplaceholder.typicode.com/posts')
+        .then((response)=>{
+            console.log(response.data);
+            this.posts = response.data
+        })       
+        .catch((error)=>{ 
+          console.log(error);
+          this.errorMsg = "erreur oups les mecs - " + error
+        })  
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
