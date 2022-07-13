e41b9a6a34b0aa93a86b315114bbaf49b4a1a402
lapinRagnar <lapinragnar@gmail.com>
2022-07-13 07:39:58 +0200

    12 - enfant components - v-model - vue dev tools Ne marche pas le $emit euuuh

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,18 @@
 <template >
-  <div>app compo</div>
-  <button  @click="showPopup=true">afficher popup</button>
-  <PopupCompo v-if="showPopup" @cacher="showPopup = false"/>
+  <div>Bonjour</div>
+  <InputCompo />
 </template>
 
 <script>
 
-import PopupCompo from './components/Popup.vue'
+import InputCompo from './components/Input.vue'
 
 export default {
   name: 'App',
-  components: {PopupCompo},
+  components: { InputCompo },
   data(){
     return {
-      showPopup: false
+      name: ''
     }    
   },
   methods: {},
@@ -25,13 +24,7 @@ export default {
 <style>
 
 #app {
-
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  /* text-align: center; */
-  color: #2c3e50;
-  margin-top: 60px;
 }
 
 
---
+++ src/components/Input.vue
@@ -0,0 +1,33 @@
+<template>
+  <input 
+    type="text" 
+    :value="modelValue"
+    @input="$emit('update:modelValue', $event.target.value)"  
+  >
+</template>
+
+<script>
+export default {
+  name: 'InputCompo',
+  props: {
+    modelValue: String
+  }
+}
+</script>
+
+<style>
+  input{
+    border:0;
+    border-bottom:1px solid #555;  
+    background:transparent;
+    width:100%;
+    padding:8px 0 5px 0;
+    font-size:16px;
+    color:rgb(143, 24, 24);
+  }
+  input:focus{
+    border:none;	
+    outline:none;
+    border-bottom:1px solid #e74c3c;	
+  }
+</style>
