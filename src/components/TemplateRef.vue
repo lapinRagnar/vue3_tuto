ba8b390af5e98b071d720b63d554afc24ef611a6
lapinRagnar <lapinragnar@gmail.com>
2022-07-14 11:20:05 +0200

    template ref avec un composant

--- src/components/TemplateRef.vue
+++ src/components/TemplateRef.vue
@@ -3,13 +3,21 @@
   <div>
     <input type="text" ref="inputRef">
   </div>
+  
+  <PostList ref="postListRef" />
+
 </template>
 
 <script>
+
+import PostList from './PostList.vue'
+
 export default {
   name: 'TemplateRef',
+  components: {PostList},
   mounted(){
     this.$refs.inputRef.focus()
+    console.log(this.$refs.postListRef)
   }
 }
 </script>
