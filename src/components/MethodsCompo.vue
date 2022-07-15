e8b78a23ab258da779d22d07a46489765122f1f5
lapinRagnar <lapinragnar@gmail.com>
2022-07-15 23:41:38 +0200

    Remplacing methods - exemple 2

--- src/components/MethodsCompo.vue
+++ src/components/MethodsCompo.vue
@@ -2,15 +2,24 @@
   <h1>avec option api</h1>
   <h3>mon nom est {{name}}</h3>
   <button @click="setName">changer le nom</button>
+
   <hr>
+  
   <h1> fonction avec setup - composition api</h1>
   <h2>{{count}}</h2>
   <button @click="incrementCount">incrementer</button>
+
+  <hr>
+
+  <h1>exemple avec - reactive et toRefs</h1>
+  <h2>{{first}} + {{last}}</h2>
+  <button @click="changeHero">change hero</button>
+
 </template>
 
 <script>
 
-import {ref} from 'vue'
+import {ref, reactive, toRefs} from 'vue'
 export default {
   name: 'MethodsCompo',
   data() {
@@ -25,14 +34,25 @@ export default {
   },
   setup(){
     const count = ref(0)
+    const state = reactive({
+        first: 'bruce',
+        last: 'wayne'
+    })
     
     function incrementCount(){
         count.value++
     }
 
+    function changeHero(){
+        state.first = 'lapin'
+        state.last = 'ragnar'
+    }
+
     return {
         count,
-        incrementCount
+        incrementCount,
+        ...toRefs(state),
+        changeHero
     }
   }
 
