c81b873cc3c0f676506e289712506ed6e38899e8
lapinRagnar <lapinragnar@gmail.com>
2022-07-17 10:10:49 +0200

    Replacing Computed property - ex 2 - composition api - reactive, toReffs, computed

--- src/components/ComputedCompo.vue
+++ src/components/ComputedCompo.vue
@@ -3,12 +3,25 @@
   <input type="text" placeholder="first name" v-model="fName">
   <input type="text" placeholder="last name" v-model="lName">
   <p>options api fullName - {{fName}} et {{lName}} => {{fullName}}</p>
-  <h1>computed</h1>
+  
+  <h1>composition api - computed</h1>
+  
+  <input type="text" placeholder="first name" v-model="refFirstName">
+  <input type="text" placeholder="last name" v-model="refLastName">
+  <p>composition api - computed fullName => {{refFullName   }}</p>
 
+
+  <h1>composition api - reactive, toReffs, computed</h1>
+  
+  <input type="text" placeholder="first name" v-model="reactiveFirstName">
+  <input type="text" placeholder="last name" v-model="reactiveLastName">
+  <p>composition api - reactive fullName => {{reactiveFullName}}</p>
 
 </template>
 
 <script>
+
+import {ref, computed, reactive, toRefs} from 'vue'
 
 export default {
   name: 'ComputedCompo',
@@ -21,6 +34,31 @@ export default {
   computed:{
     fullName(){
       return `${this.fName} -${this.lName}`
+    }
+  },
+  setup(){
+    const refFirstName = ref('')
+    const refLastName = ref('')
+
+    const refFullName = computed(function(){
+      return `${refFirstName.value} ${refLastName.value}`
+    })
+
+    const state = reactive({
+      reactiveFirstName: '',
+      reactiveLastName: '',
+    })
+
+    const reactiveFullName = computed(function(){
+      return `${state.reactiveFirstName} ${state.reactiveLastName} `
+    })
+
+    return {
+        refFirstName,
+        refLastName,
+        refFullName,
+        ...toRefs(state),
+        reactiveFullName
     }
   }
 
